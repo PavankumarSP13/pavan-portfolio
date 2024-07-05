49e360eadcc0b1fdea6cbef1116ef5d2bed5269b
PavankumarSP13 <pavankumar.sp@triconinfotech.com>
2024-07-05 12:51:42 +0530

    nav added for on click

--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -1,6 +1,6 @@
 // src/components/ImageCarousel.js
 
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 import ImageModal from './ImageModal';
 import '../styles/ImageCarousel.css';
 
@@ -55,4 +55,66 @@ const ImageCarousel = ({ images }) => {
   );
 };
 
+export default ImageCarousel; */
+
+// src/components/ImageCarousel.js
+
+import React, { useState } from 'react';
+import ImageModal from './ImageModal';
+import '../styles/ImageCarousel.css';
+
+const ImageCarousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const openModal = (index) => {
+    setCurrentIndex(index);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className="carousel">
+      <button className="carousel-button left" onClick={prevSlide}>
+        &#9664;
+      </button>
+      <div className="image-container">
+        <img
+          src={images[currentIndex]}
+          alt="profile"
+          className="carousel-image"
+          onClick={() => openModal(currentIndex)}
+        />
+      </div>
+      <button className="carousel-button right" onClick={nextSlide}>
+        &#9654;
+      </button>
+
+      <ImageModal
+        isOpen={isModalOpen}
+        imageSrc={images[currentIndex]}
+        onClose={closeModal}
+        onNext={nextSlide}
+        onPrev={prevSlide}
+      />
+    </div>
+  );
+};
+
 export default ImageCarousel;
+
--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -1,19 +1,50 @@
 // src/components/ImageModal.js
 
+// import React from 'react';
+// import '../styles/ImageModal.css';
+
+// const ImageModal = ({ isOpen, imageSrc, onClose }) => {
+//   if (!isOpen) return null;
+
+//   return (
+//     <div className="modal-overlay" onClick={onClose}>
+//       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+//         <span className="modal-close" onClick={onClose}>&times;</span>
+//         <img src={imageSrc} alt="Enlarged view" className="modal-image" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ImageModal;
+
+// src/components/ImageModal.js
+
 import React from 'react';
 import '../styles/ImageModal.css';
 
-const ImageModal = ({ isOpen, imageSrc, onClose }) => {
-  if (!isOpen) return null;
+const ImageModal = ({ isOpen, imageSrc, onClose, onNext, onPrev }) => {
+  if (!isOpen) {
+    return null;
+  }
 
   return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <span className="modal-close" onClick={onClose}>&times;</span>
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="modal-close-button" onClick={onClose}>
+          &times;
+        </button>
+        <button className="modal-nav-button left" onClick={onPrev}>
+          &#9664;
+        </button>
         <img src={imageSrc} alt="Enlarged view" className="modal-image" />
+        <button className="modal-nav-button right" onClick={onNext}>
+          &#9654;
+        </button>
       </div>
     </div>
   );
 };
 
 export default ImageModal;
+
